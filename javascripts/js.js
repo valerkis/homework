ba05d71bb28296bd28a1d9fd4548ad66b60435a5
valerkis <vrrrx@mail.ru>
2025-01-19 22:47:21 +0300

    0

---
+++ javascripts/js.js
@@ -0,0 +1,19 @@
+// // задача 1
+let str = "abcde"
+alert (str[0])
+alert (str[1])
+alert (str[4])
+
+// // задача 2
+let input1 = parseInt (prompt("Введите одно число"))
+let input2 = parseInt (prompt("Введите второе число"))
+console.log(input1 > 0 | input2 > 0);
+
+// задача 3
+let a = parseInt (prompt("Сколько Вам лет?"))
+if (a >= 18) {
+    console.log ("Совершеннолетний")
+}
+else {
+    console.log ("Несовершеннолетний")
+}
